afe14816ac02c9f0e080fbe6873206bc38786c60
SophiaLP19 <sophia.laura.pavelski.ifpr@gmail.com>
2025-11-10 16:20:30 -0300

    botao

--- js/miniProjeto.js
+++ js/miniProjeto.js
@@ -1,10 +1,28 @@
+cont = 1;
+cont1 = 1;
 function adicionarTexto(){
+
     var texto = document.getElementById("caixaTexto").value;
     var check = document.getElementById("checkUrgente").checked;
     var nota = document.createElement("p");
     nota.innerText = texto;
     nota.id = "parágrafo";
-
+    if(cont==1)
+    {
+        nota.style.color = "blue";
+        cont++;
+    }
+    else{
+        if(cont==2)
+        {
+            nota.style.color = "green";
+            cont++;
+        }
+        else{
+            nota.style.color = "purple";
+            cont=1;
+        }
+    }
     if(check)
     {
         var urgente = document.getElementById("urgentes");
@@ -14,6 +32,12 @@ function adicionarTexto(){
     {
         var nUrgente = document.getElementById("nurgentes");
         nUrgente.appendChild(nota);
+    }
+
+    function removerTexto(){
+        alert("Removendo texto...");
+        var remover = document.getElementById("parágrafo");
+        remover.remove();
     }
 
 
